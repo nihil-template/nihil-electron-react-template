5027ef4d4dbb51924e5b6853d528c562f7bd9bbb
NIHILncunia <nihil_ncunia@naver.com>
2022-05-12 12:19:02 +0900

    2022 0512 템플릿 업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { store } from '@/store';
-
-import App from './App';
-import TestPage from './pages/test';
-import TestTestPage from './pages/testtest';
+import App from '@/App';
+import TestPage from '@/pages/test';
+import TestTestPage from '@/pages/testtest';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
